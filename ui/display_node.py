fe7a941b381138a60eb1fbbb5bb4024380e6224f
oycq <49046960@qq.com>
2019-11-29 23:01:25 +0800

    network_display_node

--- ui/display_node.py
+++ ui/display_node.py
@@ -6,8 +6,11 @@ import time
 from sensor_msgs.msg import Joy
 from std_msgs.msg import String,Int16MultiArray
 import SharedArray as sa
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image
 
-
+bridge = CvBridge()
 image_path = ''
 x = {}
 mouse_x = 400
@@ -15,12 +18,13 @@ mouse_y = 400
 box_w = 50
 traking_pt1 = (0,0)
 traking_pt2 = (0,0)
+new_image = 0
 
-def mouse_callback(event,x,y,flags,param):
-    global mouse_x, mouse_y, pt1, pt2
-    if event == cv2.EVENT_MOUSEMOVE:
-        mouse_x = x
-        mouse_y = y
+#def mouse_callback(event,x,y,flags,param):
+#    global mouse_x, mouse_y, pt1, pt2
+#    if event == cv2.EVENT_MOUSEMOVE:
+#        mouse_x = x
+#        mouse_y = y
 
 def caculate_box_points(x,y,width,img_shape):
     d = width // 2
@@ -59,8 +63,9 @@ def joy_callback(a):
 #    print(a.axes[3], a.buttons[0])
 
 def image_path_callback(a):
-    global image_path, x
+    global image_path, x, new_image
     image_path = a.data
+    new_image = 1
     if a.data not in x:
         x[a.data] = sa.attach(a.data)   
 
@@ -71,28 +76,40 @@ def tracking_info_callback(a):
     
 
 if __name__ == '__main__':
+#    dispaly_image_pub = rospy.Publisher('display_image/compressed', Image, queue_size=1)
+    dispaly_image_pub = rospy.Publisher('display_image', Image, queue_size=1)
     rospy.init_node('display_node', anonymous=False)
-    rospy.Subscriber("image_path", std_msgs.msg.String, image_path_callback)
+    rospy.Subscriber("image_path/", std_msgs.msg.String, image_path_callback)
     rospy.Subscriber("joy", Joy, joy_callback)
     rospy.Subscriber('tracking_info', Int16MultiArray, tracking_info_callback)
     lockon_commmand_pub = rospy.Publisher('lockon_command_node', String, queue_size = 1)
-    cv2.namedWindow('img', cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty("img",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-    cv2.setMouseCallback('img',mouse_callback)
+#    cv2.namedWindow('img', cv2.WND_PROP_FULLSCREEN)
+#    cv2.setWindowProperty("img",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+#    cv2.setMouseCallback('img',mouse_callback)
     while not rospy.is_shutdown():
         if image_path == '':
             time.sleep(0.1)
             continue
+        t0 = time.time() * 1000
+        while not rospy.is_shutdown():
+            if new_image == 1:
+                new_image = 0
+                break
+            else:
+                time.sleep(0.0005)
         t1 = time.time() * 1000
-        #raw = cv2.imread(image_path, 0)
         raw = x[image_path].copy()
         cv2.circle(raw, (1024,768), 10,(255,0,0), 2)
         pt1,pt2 = caculate_box_points(mouse_x, mouse_y, box_w, raw.shape)
         cv2.rectangle(raw,pt1,pt2,(0,255,0),2)
         cv2.rectangle(raw,traking_pt1,traking_pt2,(0,0,255),2)
-        cv2.imshow('img',raw)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-        if key == ord(' '):
-            lockon_commmand_pub.publish('lockon %d %d %d %d'%(pt1[0], pt1[1],pt2[0],pt2[1]))
+ #       cv2.imshow('img',raw)
+        t2 = time.time() * 1000
+        ros_image = bridge.cv2_to_imgmsg(cv2.resize(raw,(640,480)), "bgr8")
+        #ros_image = bridge.cv2_to_compressed_imgmsg(cv2.resize(raw,(640,480)), "jpg")
+        dispaly_image_pub.publish(ros_image)
+        t3 = time.time() * 1000
+#        key = cv2.waitKey(1)
+        print('%7.2f %7.2f %7.2f %7.2f'%(t1-t0, t2-t1, t3-t2, t3-t0))
+#        if key == ord('q'):
+#            break
